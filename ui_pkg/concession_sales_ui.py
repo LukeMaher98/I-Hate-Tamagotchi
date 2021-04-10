12f4ef699ae5ef55d9094b45185c43a8d0f6febc
Lennaayy <lennyeum@gmail.com>
2021-04-10 15:49:36 +0100

    update values added for concession sales

--- ui_pkg/concession_sales_ui.py
+++ ui_pkg/concession_sales_ui.py
@@ -10,4 +10,4 @@ concession_sales_screen = concession_sales_list.generate_list()
 
 adminLayout = [[sg.Text("TheAter Concession Sales")], 
              [sg.Listbox(concession_sales_screen, size=(100, len(concession_sales_screen)), key='-List-', enable_events=True)],
-             [sg.Button('Back To Menu')]]
+             [sg.Button('Update Values'), sg.Button('Back To Menu')]]
