31de4f6960f06d2f0ac3acabb5415416f85e88b3
LukeMaher98 <98lukemaher@gmail.com>
2021-04-14 18:16:30 +0100

    Bridge and Memento integrated with updated Builder

--- entities/listings.py
+++ entities/listings.py
@@ -1,73 +1,75 @@
 import abc
+from entities import listing_bridge as lb
+
+movie_format = lb.MovieFormat()
+concession_format = lb.ConcessionFormat()
 
 
-class Listing(metaclass=abc.ABCMeta):
+class List(metaclass=abc.ABCMeta):
     @abc.abstractclassmethod
     def generate_list(self):
         pass
 
-class MovieList(Listing):
+
+class MovieList(List):
     def __init__(self, filename):
         self.filename = filename
 
     def generate_list(self):
         full_list = []
         for file in self.filename:
-            output = ""
-            movie_info = file.get_movie_info()
-            output +="Screen " +movie_info[1]+ " - "
-            output += " "+ movie_info[0] + ":"
-            i = 0
-            while i < len(movie_info[2]):
-                output += " "+ movie_info[2][i]
-                i += 1
-            full_list.append(output)
+            m_listing = lb.ProductListing(
+                movie_format, file)
+            full_list.append(m_listing.get_listing())
         return full_list
 
-class ConcessionList(Listing):
+
+class ConcessionList(List):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate_list(self):
+        full_list = []
+        for file in self.filename[0]:
+            full_list.append(lb.ProductListing(
+                concession_format, file).get_listing())
+        return full_list
+
+    def generate_dict(self):
+        full_dict = {}
+        for file in self.filename[0]:
+            concession = lb.ProductListing(
+                concession_format, file).get_listing(True)
+            full_dict[concession.specification()] = concession
+        return full_dict
+
+
+class ConcessionSalesList(List):
     def __init__(self, filename):
         self.filename = filename
 
     def generate_list(self):
         full_list = []
         for file in self.filename:
-            output = ""
-            concession_info = file.get_concession_info()
-            output +=concession_info[0]
-            output += ":"+ concession_info[1]
-            full_list.append(output)
+            full_list.append(lb.SalesListing(
+                concession_format, file).get_listing())
         return full_list
 
-class ConcessionSalesList(Listing):
+
+class TicketSalesList(List):
     def __init__(self, filename):
         self.filename = filename
 
     def generate_list(self):
         full_list = []
         for file in self.filename:
-            output = ""
-            concession_sales_info = file.get_concession_sales_info()
-            output += concession_sales_info[0]
-            output += " "+ concession_sales_info[1]
-            full_list.append(output)
+            full_list.append(lb.SalesListing(
+                movie_format, file).get_listing())
         return full_list
 
-class TicketSalesList(Listing):
-    def __init__(self, filename):
-        self.filename = filename
-
-    def generate_list(self):
-        full_list = []
-        for file in self.filename:
-            output = ""
-            ticket_sales_info = file.get_ticket_sales_info()
-            output +=ticket_sales_info[0]
-            output += " "+ ticket_sales_info[1]
-            full_list.append(output)
-        return full_list
 
 class ListFactory:
-    def create_list(self,type,list_info):
+    def create_list(self, type, list_info):
         if type == "movie":
             return MovieList(list_info)
         if type == "concession":
@@ -76,5 +78,6 @@ class ListFactory:
             return ConcessionSalesList(list_info)
         if type == "ticket sale":
             return TicketSalesList(list_info)
+
 
 list_factory = ListFactory()
--- logic_pkg/concessions_logic.py
+++ logic_pkg/concessions_logic.py
@@ -1,84 +1,83 @@
 import PySimpleGUI as sg
 from controllers import ui_controller, logic_controller
 from entities import concession_builder as cb
+from entities import listings
 import requests
 from utils import utils
 
 
-concessions_db = utils.read_file("databases/concessions_db.txt")
-popcornBuilder = cb.PopcornBuilder()
-nachoBuilder = cb.NachoBuilder()
-colaBuilder = cb.ColaBuilder()
-hotdogBuilder = cb.HotDogBuilder()
-director = cb.Director()
-concessions_dict = {}
+concessions_info = utils.read_file("databases/concessions_db.txt")
 
-director.setBuilder(popcornBuilder)
-popcorn = director.getConcession()
-concessions_dict[popcorn.specification()] = popcorn
-
-director.setBuilder(nachoBuilder)
-nacho = director.getConcession()
-concessions_dict[nacho.specification()] = nacho
-
-director.setBuilder(colaBuilder)
-cola = director.getConcession()
-concessions_dict[cola.specification()] = cola
-
-director.setBuilder(hotdogBuilder)
-hotdog = director.getConcession()
-concessions_dict[hotdog.specification()] = hotdog
-
+concessions_list = listings.list_factory.create_list(
+    "concession", concessions_info)
+concessions_dict = concessions_list.generate_dict()
 
 
 def concessionsEventLoop(window, event, values):
     if event == 'Back To Menu':
         backToMenu()
     if event == 'Purchase Concessions':
-        sg.popup(f'{"Subtotal: "}{logic_controller.logic.get_concessions_subtotal()}{"e"}')
         concessions = logic_controller.logic.get_concessions_basket()
-        for item in concessions:
+        sg.popup(f'{"Subtotal: "}{concessions["subtotal"]}{"e"}')
+        for item in concessions["items"]:
             buyConcession(item)
         emptyBasket(window)
         backToMenu()
+    if event == 'Undo Action':
+        undo_basket(window)
+    if event == 'Redo Action':
+        redo_basket(window)
     if event == '-LIST-':
         amount = sg.popup_get_text("Select Amount")
         try:
             amount = int(amount)
             if amount > 0:
-                for i in range(amount):
-                    concession_object = concessions_dict[values['-LIST-'][0]]
-                    addToConcessionsBasket(window, concession_object)
+                concession_object = concessions_dict[values['-LIST-'][0]]
+                addToConcessionsBasket(window, concession_object, amount)
             else:
                 sg.popup("Select a value greater than 0")
         except:
             sg.popup("Select a numeric value")
     if event == '-BASKET-':
-        concession_object = concessions_dict[values['-LIST-'][0]]
+        concession_object = concessions_dict[values['-BASKET-'][0]]
         removeFromConcessionsBasket(window, concession_object)
+
 
 def backToMenu():
     ui_controller.ui.get_current_ui().Hide()
     ui_controller.ui.open_main_menu_user_ui()
     logic_controller.logic.set_main_menu_user_loop()
 
-def addToConcessionsBasket(window, concession_object):
-    concessions = logic_controller.logic.get_concessions_basket()
-    concessions.append(concession_object.getName())
-    logic_controller.logic.set_concessions_basket(concessions)
-    subtotal = logic_controller.logic.get_concessions_subtotal()
-    subtotal += concession_object.getPrice()
-    logic_controller.logic.set_concessions_subtotal(subtotal)   
-    window['-BASKET-'].update(logic_controller.logic.get_concessions_basket())
+
+def addToConcessionsBasket(window, concession_object, amount):
+    basket = logic_controller.logic.get_concessions_basket()
+    for i in range(amount):
+        basket["items"].append(concession_object.specification())
+        basket["subtotal"] += concession_object.getPrice()
+    logic_controller.logic.set_concessions_basket(basket)
+    window['-BASKET-'].update(
+        logic_controller.logic.get_concessions_basket()["items"])
+
 
 def removeFromConcessionsBasket(window, concession_object):
-    concessions = logic_controller.logic.get_concessions_basket()
-    concessions.remove(concession_object.getName())
-    logic_controller.logic.set_concessions_basket(concessions)
-    subtotal = logic_controller.logic.get_concessions_subtotal()
-    subtotal -= concession_object.getPrice()
-    logic_controller.logic.set_concessions_subtotal(subtotal) 
-    window['-BASKET-'].update(logic_controller.logic.get_concessions_basket())
+    basket = logic_controller.logic.get_concessions_basket()
+    basket["items"].remove(concession_object.specification())
+    basket["subtotal"] -= concession_object.getPrice()
+    logic_controller.logic.set_concessions_basket(basket)
+    window['-BASKET-'].update(
+        logic_controller.logic.get_concessions_basket()["items"])
+
+
+def undo_basket(window):
+    logic_controller.logic.undo_concessions_basket()
+    window['-BASKET-'].update(
+        logic_controller.logic.get_concessions_basket()["items"])
+
+
+def redo_basket(window):
+    logic_controller.logic.redo_concessions_basket()
+    window['-BASKET-'].update(
+        logic_controller.logic.get_concessions_basket()["items"])
 
 
 def buyConcession(concession):
@@ -96,9 +95,6 @@ def buyConcession(concession):
     if not found:
         concessionSales += concession+",1,\n"
 
-    subtotal = 0
-    logic_controller.logic.set_concessions_subtotal(subtotal)
-
     # requests.post("https://logs-01.loggly.com/inputs/990e729b-d1a0-4ad1-a774-78d9c11a93c7/tag/http/", json={
     #         "ConcessionPurchased": "Success",
     #     })
@@ -106,8 +102,8 @@ def buyConcession(concession):
     db = open("databases/concession_sales_db.txt", "w")
     db.write(concessionSales)
 
-def emptyBasket(window):
-    concessions = []
-    logic_controller.logic.set_concessions_basket(concessions)
-    window['-BASKET-'].update(logic_controller.logic.get_concessions_basket())
 
+def emptyBasket(window):
+    logic_controller.logic.empty_concessions_basket()
+    window['-BASKET-'].update(
+        logic_controller.logic.get_concessions_basket()["items"])
